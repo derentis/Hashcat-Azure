dab9619fb5b85e49eb5b4be67bc69452101d8460
carlmon <450101+carlmon@users.noreply.github.com>
2020-06-17 12:21:12 +0200

    Switch from GRID to CUDA driver

--- azure-entrypoint.sh
+++ azure-entrypoint.sh
@@ -9,12 +9,15 @@ blacklist nouveau
 blacklist lbm-nouveau
 EOT
 
-# Set up GRID drivers
-wget -O NVIDIA-Linux-x86_64-grid.run https://go.microsoft.com/fwlink/?linkid=874272
-chmod u+x NVIDIA-Linux-x86_64-grid.run
-./NVIDIA-Linux-x86_64-grid.run -s>>error
-cp /etc/nvidia/gridd.conf.template /etc/nvidia/gridd.conf
-sed -e "\$aIgnoreSP=FALSE" /etc/nvidia/gridd.conf
+# Install GRID driver
+# wget https://storage.googleapis.com/nvidia-drivers-us-public/GRID/GRID10.2/NVIDIA-Linux-x86_64-440.87-grid.run
+# sh NVIDIA-Linux-x86_64-440.87-grid.run -s>>error
+# cp /etc/nvidia/gridd.conf.template /etc/nvidia/gridd.conf
+# sed -e "\$aIgnoreSP=FALSE" /etc/nvidia/gridd.conf
+
+# Install CUDA driver & toolkit
+wget http://developer.download.nvidia.com/compute/cuda/11.0.1/local_installers/cuda_11.0.1_450.36.06_linux.run
+sh cuda_11.0.1_450.36.06_linux.run --driver --toolkit --silent>>error
 
 # Install Hashcat
 wget https://hashcat.net/files/hashcat-6.0.0.tar.gz
