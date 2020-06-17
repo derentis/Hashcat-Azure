06e69241bcd8af30b66fd7b99ed9ac88d852458e
carlmon <450101+carlmon@users.noreply.github.com>
2020-06-17 10:26:35 +0200

    Update hashcat to 6.0.0.

--- azure-entrypoint.sh
+++ azure-entrypoint.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 # Install required packages
-apt-get update 2>error
-apt-get install -y build-essential 2>error # Not installing 'ubuntu-desktop'
+apt update && apt install build-essential -y 2>>error # Not installing 'ubuntu-desktop'
 
 # Blacklist nouveau drivers
-cat <<EOT >> /etc/modprobe.d/nouveau.conf
+cat >> /etc/modprobe.d/nouveau.conf <<EOT
 blacklist nouveau
 blacklist lbm-nouveau
 EOT
 
 # Set up GRID drivers
 wget -O NVIDIA-Linux-x86_64-grid.run https://go.microsoft.com/fwlink/?linkid=874272
-chmod +x NVIDIA-Linux-x86_64-grid.run
-./NVIDIA-Linux-x86_64-grid.run -s>error
+chmod u+x NVIDIA-Linux-x86_64-grid.run
+./NVIDIA-Linux-x86_64-grid.run -s>>error
 cp /etc/nvidia/gridd.conf.template /etc/nvidia/gridd.conf
 sed -e "\$aIgnoreSP=FALSE" /etc/nvidia/gridd.conf
 
 # Install Hashcat
-wget https://hashcat.net/files/hashcat-5.1.0.tar.gz
-tar -xf hashcat-5.1.0.tar.gz
-cd hashcat-5.1.0
+wget https://hashcat.net/files/hashcat-6.0.0.tar.gz
+tar -xf hashcat-6.0.0.tar.gz
+cd hashcat-6.0.0
 make && make install
 
 # Install John the Ripper
